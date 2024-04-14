38de0569d0eaf4dedd31ff212630a6dd512edaa1
NaTT <1103625169lixuan@gmail.com>
2024-04-14 22:51:54 +1000

    upload prac for Tree

---
+++ prac/BSTreeGetSmallest/BSTreeGetSmallest.c
@@ -0,0 +1,12 @@
+
+#include "BSTree.h"
+
+#include <stdlib.h>
+
+BSTree BSTreeGetSmallest(BSTree t) {
+	if (t == NULL) return NULL;
+	if (t->left == NULL) return t;
+	
+	return BSTreeGetSmallest(t->left);
+}
+
